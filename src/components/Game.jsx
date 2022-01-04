5e2dcb418c10fac8f531eef70a54ecb15d27e834
Yevgeny-Sh <yevgst7@gmail.com>
2022-01-04 11:32:27 +0200

    ui defence seems to work also with strong suit

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -88,7 +88,9 @@ export default function Game() {
           let index = newArr.indexOf(element);
           newArr.splice(index, 1);
           setPlayer1Hand(newArr);
-          setTable((prevArray) => [...prevArray, element]);
+          setTimeout(() => {
+            setTable((prevArray) => [...prevArray, element]);
+          }, 500);
         }
       });
     }
@@ -148,7 +150,7 @@ export default function Game() {
 
   useEffect(() => {
     const tryToDefend = () => {
-      if (deck.length || (player1Hand.length && player1Hand.length)) {
+      if (deck.length || (player1Hand.length && player2Hand.length)) {
         let counterOfMatchesToThrow = 0;
         let indexOfMatchToThrow = -1;
         if (table.length && player2Hand.length) {
@@ -163,9 +165,10 @@ export default function Game() {
           });
           //assuming ui found a single card to defend with
           if (counterOfMatchesToThrow === 1) {
-            console.log("= 1");
-            console.log("defended with");
-            console.log(player2Hand[indexOfMatchToThrow]);
+            console.log(
+              "ui found single card to edfend with",
+              player2Hand[indexOfMatchToThrow]
+            );
             let newArrOfUiCards = player2Hand;
             newArrOfUiCards.splice(indexOfMatchToThrow, 1);
             setPlayer2Hand(newArrOfUiCards);
@@ -182,61 +185,65 @@ export default function Game() {
             //TODO make more complicated logic later
             let newArrOfUiCards = player2Hand;
             newArrOfUiCards.splice(indexOfMatchToThrow, 1);
-            //setPlayer2Hand(newArrOfUiCards);
             //draw new card from deck
             if (deck.length > 0 && player2Hand.length < 6) {
               let drawnCardFromDeck = deck.pop();
               setPlayer2Hand([...newArrOfUiCards, drawnCardFromDeck]);
             }
             setTable([]);
-            //!draw new card from deck repeats itself 3 times, try to extract to function
-            //if no cards found to defend
-          } else if (counterOfMatchesToThrow === 0) {
-            //! check for strong suit
-            if (table[0].suit !== STRONG_SUIT) {
-              //!something here doesnt work
-              // player2Hand.forEach((element) => {
-              //   if (element.suit === STRONG_SUIT) {
-              //     indexOfMatchToThrow = player2Hand.indexOf(element);
-              //     let newArrOfUiCards = player2Hand;
-              //     newArrOfUiCards.splice(indexOfMatchToThrow, 1);
-              //     setPlayer2Hand(newArrOfUiCards);
-              //     setTable([]);
-              //     //draw card from deck
-              //     let drawnCardFromDeck = deck.pop();
-              //     let prePlayer2Hand = player2Hand;
-              //     setPlayer2Hand([...prePlayer2Hand, drawnCardFromDeck]);
-              //     //drawCardFromDeckForPlayer2();
-              //   }
-              // });
-              let FoundStrongSuitCard = player2Hand.find((elm) => {
-                return elm.suit === STRONG_SUIT;
-              });
-              indexOfMatchToThrow = player2Hand.indexOf(FoundStrongSuitCard);
+            //! no reg card to defend
+          } else if (
+            counterOfMatchesToThrow === 0 &&
+            table[0].suit !== STRONG_SUIT
+          ) {
+            //! but try to defend with kozir
+            let FoundStrongSuitCard = player2Hand.find(
+              (elm) => elm.suit === STRONG_SUIT
+            );
+            // if found strong suit in UI hand
+            if (FoundStrongSuitCard !== undefined) {
+              console.log("UI has strong suit card to defend with");
+              let indexOfMatchToThrow =
+                player2Hand.indexOf(FoundStrongSuitCard);
+              console.log("found strong card to throw :", FoundStrongSuitCard);
               let newArrOfUiCards = player2Hand;
               newArrOfUiCards.splice(indexOfMatchToThrow, 1);
+              setPlayer2Hand(newArrOfUiCards);
+              setTable([]);
               //now draw card
               if (deck.length > 0 && player2Hand.length < 6) {
+                console.log("if is true,drawing");
                 let drawnCardFromDeck = deck.pop();
                 setPlayer2Hand([...newArrOfUiCards, drawnCardFromDeck]);
               }
-
+              //no strong suit card found
+            } else if (FoundStrongSuitCard === undefined) {
+              console.log("no strong suit card to UI, ui needs to take card");
+              let newArr2 = player2Hand;
+              newArr2.push(table[0]);
+              setPlayer2Hand(newArr2);
               setTable([]);
             }
-            //!what does that block do?
-            // if (indexOfMatchToThrow <= 0) {
-            //   let newArr = player2Hand;
-            //   newArr.push(table[0]);
-            //   setPlayer2Hand(newArr);
-            //   setTable([]);
-            // }
+            //! strong suit on table, and ui cant defend
+          } else if (
+            counterOfMatchesToThrow === 0 &&
+            table[0].suit === STRONG_SUIT
+          ) {
+            //take card
+            let newArr3 = player2Hand;
+            newArr3.push(table[0]);
+            setPlayer2Hand(newArr3);
+            setTable([]);
           }
         }
+        //if deck is empty or a player has no cards in hand
       } else {
         checkWinner();
       }
+      //end of tryToDefend()
     };
     tryToDefend();
+    //end of useEffect
   });
 
   //!check if there is winnr function
@@ -249,19 +256,6 @@ export default function Game() {
       return;
     }
   };
-
-  // const drawCardFromDeckForPlayer2 = () => {
-  //   if (deck.length > 0 && player2Hand.length < 6) {
-  //     console.log("drawing card after defence");
-  //     //!draw card doesnt work
-  //     let drawnCardFromDeck = deck.pop();
-  //     let prePlayer2Hand = player2Hand;
-  //     //setPlayer2Hand([...prePlayer2Hand, drawnCardFromDeck]);
-  //     setPlayer2Hand([...prePlayer2Hand, drawnCardFromDeck]);
-  //     console.log("draw successfull??");
-  //     console.log(player2Hand);
-  //   }
-  // };
 
   return (
     <div>
